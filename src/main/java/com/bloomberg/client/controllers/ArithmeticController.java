adc935483540891c79e45790820ceff3cae8cfab
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-05 23:01:16 +0100

    adding request body to post

--- src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
+++ src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -18,15 +20,15 @@ public class ArithmeticController {
     @Autowired
     public ArithmeticController(WebClient.Builder webClientBuilder) {
         //TODO set uri in config file
-        webClient = webClientBuilder.baseUrl("http://arithmetic-client/operation").defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+        webClient = webClientBuilder
+                        .baseUrl("http://arithmetic-client/operation")
+                        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
     }
 
     @RequestMapping("/arithmetic")
-    public Mono<String> arithmetic() {
-        logger.info("Access /arithmetic");
-        var result = webClient.post().retrieve().bodyToMono(String.class);
-
-        logger.info("Access /arithmetic2");
+    public Mono<String> arithmetic(@RequestBody String body) {
+        //TODO try to bind body as object maybe?
+        var result = webClient.post().body(BodyInserters.fromValue(body)).retrieve().bodyToMono(String.class);
         return result;
 
     /*    return loadBalancedWebClientBuilder.build().post().uri("http://arithmetic-client/operation")
