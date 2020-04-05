1a13719d54e2e69fe476162b349e9f6865b20472
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-05 22:51:25 +0100

    removed unnecessary imports and setting up properly content type and baseurl

--- src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
+++ src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
@@ -1,30 +1,30 @@
 package com.bloomberg.client.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class ArithmeticController {
     private static Logger logger = LoggerFactory.getLogger(ArithmeticController.class);
-
+    private WebClient webClient;
 
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    public ArithmeticController(WebClient.Builder webClientBuilder) {
+        //TODO set uri in config file
+        webClient = webClientBuilder.baseUrl("http://arithmetic-client/operation").defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+    }
 
     @RequestMapping("/arithmetic")
-    public Mono<String> arithmetic(@RequestBody String test) {
+    public Mono<String> arithmetic() {
         logger.info("Access /arithmetic");
-        var result =  webClientBuilder.build().post().uri("http://arithmetic-client/operation").retrieve().bodyToMono(String.class);
+        var result = webClient.post().retrieve().bodyToMono(String.class);
 
         logger.info("Access /arithmetic2");
         return result;
