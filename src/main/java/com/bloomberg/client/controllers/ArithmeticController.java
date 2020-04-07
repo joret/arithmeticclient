5c3f4cb0adabe1229f0287ef830e8e4e745285d7
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-07 14:21:39 +0100

    adding proper fallback to http://api.mathjs.org/v4/

--- src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
+++ src/main/java/com/bloomberg/client/controllers/ArithmeticController.java
@@ -1,5 +1,7 @@
 package com.bloomberg.client.controllers;
 
+import com.bloomberg.client.models.Operation;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -23,6 +26,8 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class ArithmeticController {
     private static Logger logger = LoggerFactory.getLogger(ArithmeticController.class);
     private WebClient webClient;
+    private WebClient loadBalancedWeb;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     //TODO set in config
     private final String uri = "http://arithmetic-service/operation";
@@ -30,14 +35,22 @@ public class ArithmeticController {
     private final int timeout = 20000;
 
     @Autowired
-    public ArithmeticController(WebClient.Builder webClientBuilder) {
-        webClient = webClientBuilder
+    public ArithmeticController(WebClient.Builder loadBalancedWebClientBuilder, WebClient.Builder webClientBuilder) {
+        loadBalancedWeb = loadBalancedWebClientBuilder
                         .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .filters(exchangeFilterFunctions -> {
                             exchangeFilterFunctions.add(logRequest());
                             exchangeFilterFunctions.add(logResponse());
                         })
                         .build();
+
+        webClient = webClientBuilder
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filters(exchangeFilterFunctions -> {
+                    exchangeFilterFunctions.add(logRequest());
+                    exchangeFilterFunctions.add(logResponse());
+                })
+                .build();
     }
 
     private ExchangeFilterFunction logRequest() {
@@ -80,12 +93,14 @@ public class ArithmeticController {
     @RequestMapping("/arithmetic")
     public Mono<String> arithmetic(@RequestBody String body) {
 
-        var result = webClient.post().uri(uri).body(BodyInserters.fromValue(body))
+        var result = loadBalancedWeb.post().uri(uri).body(BodyInserters.fromValue(body))
                 .retrieve()
                 .bodyToMono(String.class)
                 .onErrorResume(WebClientResponseException.class,
-                    //Fallback only on 500 or Timeout
-                    ex -> (ex.getStatusCode() == INTERNAL_SERVER_ERROR || ex.getStatusCode() == HttpStatus.REQUEST_TIMEOUT)
+                    //Fallback only on 500, 503 or Timeout
+                    ex -> (ex.getStatusCode() == INTERNAL_SERVER_ERROR
+                            || ex.getStatusCode() == HttpStatus.REQUEST_TIMEOUT
+                            || ex.getStatusCode() == HttpStatus.SERVICE_UNAVAILABLE)
                             ?  fallback(body): Mono.error(ex))
                 .timeout(Duration.ofMillis(timeout));
 
@@ -94,12 +109,19 @@ public class ArithmeticController {
     }
 
     private Mono<String> fallback(String body){
+        logger.info("POST to Fallback----------");
 
-                    logger.info("Fallback");
-                    var result = webClient.post().uri(uriFallback).body(BodyInserters.fromValue(body))
-                            .retrieve()
-                            .bodyToMono(String.class).timeout(Duration.ofMillis(timeout));
+        try {
+            Operation operation = objectMapper.readValue(body, Operation.class);
+            String fallbackExpression = operation.toFallbackJsonObject();
+            var result = webClient.post().uri(uriFallback).body(BodyInserters.fromValue(fallbackExpression))
+                    .retrieve()
+                    .bodyToMono(String.class).timeout(Duration.ofMillis(timeout));
 
-                    return result;
+            return result;
+        } catch (IOException e) {
+            return Mono.error(e);
+        }
+
     }
 }
