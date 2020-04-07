3bccba722fc82c1cfd2f2dc0d5cac7e2ac8be491
marcelo.flores <marcelo.floresman@gmail.com>
2020-04-07 14:22:10 +0100

    adding models to interact with main api and fallback api

--- src/main/java/com/bloomberg/client/models/FallbackExpression.java
+++ src/main/java/com/bloomberg/client/models/FallbackExpression.java
@@ -1,4 +1,12 @@
 package com.bloomberg.client.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class FallbackExpression {
+    List<String> expr;
 }
--- src/main/java/com/bloomberg/client/models/Operation.java
+++ src/main/java/com/bloomberg/client/models/Operation.java
@@ -1,4 +1,33 @@
 package com.bloomberg.client.models;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Operation {
+    private Character operation;
+    private List<Double> numbers;
+
+    public String toFallbackJsonObject(){
+        FallbackExpression result = new FallbackExpression(null);
+
+        if (numbers != null && numbers.size() == 2 && !Character.isWhitespace(operation)){
+            var expressionList = new ArrayList<String>();
+            expressionList.add( "" + numbers.get(0) + operation + numbers.get(1));
+            result = new FallbackExpression(expressionList);
+
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            String str = objectMapper.writeValueAsString(result);
+
+            return str;
+        } catch(Exception e){
+            return "";
+        }
+    }
 }
